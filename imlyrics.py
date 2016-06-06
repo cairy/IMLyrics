83ce20e1cf129ce129b9d4419b8b4b3bcae2b64f
cairy <cairyli@live.cn>
2016-06-06 17:14:28 +0800

    可以从命令行运行
    修改文件编码和读取文件时文件名的编码处理
    增加交互式运行

--- imlyrics.py
+++ imlyrics.py
@@ -1,8 +1,7 @@
-"""Written By cairy """
+﻿"""Written By cairy """
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 import sys
-sys.path.append(r'D:\python_code\lib')
 import glob
 import eyed3
 import urllib2
@@ -11,25 +10,33 @@ from bs4 import BeautifulSoup
 from searchlrcfrombaidu import searchLrcFromBaidu
 
 def main(d):
-    song_names= glob.glob(d.decode('gb18030'))
+    song_names= glob.glob(d)
     for name in song_names:
-        print ((name)).encode('gb18030')
+        print(name)
         basename=os.path.basename(name)
-        basename=basename.encode('gb18030')
+        basename=basename.decode('gb18030').encode('utf8')
         basename= basename.replace('.mp3','')
         
-        audiofile = eyed3.load((name))
-        lyrics=searchLrcFromBaidu(basename +' ���')
+        audiofile = eyed3.load(name)
+        lyrics=searchLrcFromBaidu(basename +' 歌词')
         if lyrics:
             audiofile.tag.lyrics.set(lyrics )
             audiofile.tag.save()
-            print('�ɹ�')
+            print('成功'.decode('utf8').encode('gb18030'))
             with open (name.replace('+',' ')+'.txt','w') as f:
                 f.write(lyrics.encode('gb18030'))
         else:
             with open ('errorfile.txt','a') as f:
-                f.write('\n'+name.encode('gb18030'))
+                f.write('\n'+name)
                 
 if __name__=='__main__':
-    d=r"F:\Ipad_OK\*.mp3"
-    main(d)
+    #d=r"F:\Ipad_OK\*.mp3"
+    d=''
+    if len(sys.argv)==2:
+        d=sys.argv[1]
+    if not d:
+        d=raw_input("请输入目录名:".decode('utf8').encode('gb18030'))    
+    if os.path.isdir(d):
+        print d
+        d=os.path.join(d,'*.mp3')
+        main(d)
